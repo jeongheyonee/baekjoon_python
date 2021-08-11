51c5e5d7633624cc944e400d035d1cf7b46e387d
jeonghyeonee <33801356+jeonghyeonee@users.noreply.github.com>
2021-08-12 00:33:06 +0900

    2021.08.11.
    기본 수학2

--- 2021.08.11/4948.py
+++ 2021.08.11/4948.py
@@ -1,5 +1,28 @@
 import math
 
+# while True:
+# 시간초과임...
+#     n = int(input())
+#
+#     if n == 0:
+#         break
+#
+#     cnt = 0
+#
+#     for i in range(n+1, 2*n+1):
+#         if i == 1:
+#             continue
+#         elif i == 2:
+#             cnt += 1
+#             continue
+#         else:
+#             for j in range(2, int(math.sqrt(i))+1):
+#                 if i%j == 0:
+#                     break
+#             else:
+#                 cnt += 1
+#     print(cnt)
+
 def postulate(n):
     prime = [True for _ in range(2*n+1)]
     prime[0] = False    # 1은 소수가 아님
@@ -7,19 +30,14 @@ def postulate(n):
     for i in range(2, int(math.sqrt(2*n))+1):
         if prime[i] == True:
             j = 2
-            while i*j <= n:
+            while i*j <= 2*n:
                 prime[i*j] = False
                 j += 1
-    cnt = 0
-    for i in range(n, 2*n+1):
-        if i > 1:
-            if prime[i] == True:
-                cnt += 1
-    return prime
+    return [i for i in range(n+1, 2*n+1) if prime[i] == True]
 
 while True:
     n = int(input())
     if n == 0:
         break
     else:
-        print(postulate(n))
+        print(len(postulate(n)))

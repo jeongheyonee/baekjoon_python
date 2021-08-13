f1c01dae41f7605aae575cb619a754c09143259d
jeonghyeonee <33801356+jeonghyeonee@users.noreply.github.com>
2021-08-13 20:28:30 +0900

    2021.08.11.
    기본 수학2

--- 2021.08.11/1929.py
+++ 2021.08.11/1929.py
@@ -4,19 +4,19 @@ import math
 m, n = map(int, input().split())
 
 prime = [True for _ in range(n+1)]
-#
-# for i in range(2, int(math.sqrt(n))+1):
-#     if prime[i] == True:
-#         j = 2
-#         while i*j <= n:
-#             prime[i*j] = False
-#             j += 1
-#
-# for i in range(m, n+1):
-#     if i>1:
-#         if prime[i] == True:
-#             print(i)
-#
+
+for i in range(2, int(math.sqrt(n))+1):
+    if prime[i] == True:
+        j = 2
+        while i*j <= n:
+            prime[i*j] = False
+            j += 1
+
+for i in range(m, n+1):
+    if i>1:
+        if prime[i] == True:
+            print(i)
+
 # # for i in prime:
 # #     cnt = 0
 # #     for j in range(1, i+1):

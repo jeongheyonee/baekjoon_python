a30b39258f70a41442d28d16f9c3b71a090156c4
jeonghyeonee <jlh1218@naver.com>
2021-09-25 22:13:16 +0900

    2021.09.24.

---
+++ 2021.09/2021.09.24/10773.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+k = int(input())
+
+num = []
+
+for _ in range(k):
+    n = int(input())
+
+    if n == 0:
+        num.pop()
+    else:
+        num.append(n)
+
+print(sum(num))

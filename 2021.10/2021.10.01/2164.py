46f20aaeb9fc06d3d7bb170764a25561bf6aea84
jeonghyeonee <jlh1218@naver.com>
2021-10-01 18:32:19 +0900

    2021.10.01.

---
+++ 2021.10/2021.10.01/2164.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+
+card = deque([i for i in range(1, n+1)])
+
+while len(card) > 1:
+    card.popleft()
+    card.append(card[0])
+    card.popleft()
+
+print(card[0])

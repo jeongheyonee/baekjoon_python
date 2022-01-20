e04627a75f272db7180e144cc5e001022d173090
jeonghyeonee <jlh1218@naver.com>
2022-01-20 22:33:11 +0900

    2022.01.20. 11279 - heap

--- 2022/2022.01/2022.01.20/11279.py
+++ 2022/2022.01/2022.01.20/11279.py
@@ -1,18 +1,22 @@
+# heap을 어떻게 사용할 것인가?
+# heap 직접 구현으로 다시 풀어보기!!! 꼭!!!
+# https://hooongs.tistory.com/130
+
 import sys
+import heapq
 
 input = sys.stdin.readline
 
 n = int(input())
-arr = []
+heap = []
 
 for _ in range(n):
     x = int(input())
 
-    if x != 0:
-        arr.append(x)
-    else:
-        if len(arr) == 0:
+    if x == 0:
+        try:
+            print(-1*heapq.heappop(heap))
+        except:
             print(0)
-            continue
-        print(max(arr))
-        arr.remove(max(arr))
+    else:
+        heapq.heappush(heap, -x)

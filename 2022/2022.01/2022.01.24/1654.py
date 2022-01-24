f9261a337e10939495ad61dee47cfc0e2a25fa71
jeonghyeonee <jlh1218@naver.com>
2022-01-24 23:11:28 +0900

    2022.01.24. 1654

--- 2022/2022.01/2022.01.24/1654.py
+++ 2022/2022.01/2022.01.24/1654.py
@@ -1,6 +1,21 @@
 # 어떻게 풀어야 하지,,?
-k, n = map(int, input().split())
-s = 0
+import sys
 
-for _ in range(k):
-    x = int(input())
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+
+arr = [int(input()) for _ in range(k)]
+start, end = 1, max(arr)
+
+while start <= end:
+    mid = (start + end) // 2
+    s = 0
+    for i in arr:
+        s += i // mid
+    if s >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)

2e175311ec4e0305260115bcb2945580a67c153e
jeonghyeonee <jlh1218@naver.com>
2022-01-29 21:03:44 +0900

    2022.01.29.

---
+++ 2022/2022.01/2022.01.29/1927.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    x = int(input())
+
+    if x == 0:
+        try:
+            print(heapq.heappop(heap))
+        except:
+            print(0)
+    else:
+        heapq.heappush(heap, x)

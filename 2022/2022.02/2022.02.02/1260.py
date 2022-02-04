a17801dd21ee8210103cf3875b2cee3b75d02b02
jeonghyeonee <jlh1218@naver.com>
2022-02-04 23:53:25 +0900

    2022.02.04.

--- 2022/2022.02/2022.02.02/1260.py
+++ 2022/2022.02/2022.02.02/1260.py
@@ -3,7 +3,7 @@
 # bfs: 너비 우선 탐색
 # 최대한 넓게 이동한 다음, 더 이상 갈 수 없을 때 아래로 이동
 # 오,,,어떻게 풀어야 하는지 이것도 모르겠다!
-# https://goplanit.site/42.%20Algorithm(1260_py)/ 
+# https://goplanit.site/42.%20Algorithm(1260_py)/
 import sys
 from collections import deque
 
---
+++ 2022/2022.02/2022.02.04/2606.py
@@ -0,0 +1,5 @@
+n = int(input())    #컴퓨터의 수
+m = int(input())    #컴퓨터 쌍의 수
+
+for _ in range(n):
+    a, b = map(int, input().split())

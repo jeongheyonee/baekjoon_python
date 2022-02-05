67f022491905c3e3365e7e2ce66b77e47f0378b2
jeonghyeonee <jlh1218@naver.com>
2022-02-05 21:41:05 +0900

    2022.02.05.

--- 2022/2022.02/2022.02.04/2606.py
+++ 2022/2022.02/2022.02.04/2606.py
@@ -1,5 +1,29 @@
+import sys
+
+def dfs(n):
+    global cnt
+    visit[n] = True
+    for i in computer[n]:
+        if not visit[i]:
+            cnt+=1
+            dfs(i)
+    return cnt
+
+input = sys.stdin.readline
+
 n = int(input())    #컴퓨터의 수
 m = int(input())    #컴퓨터 쌍의 수
 
-for _ in range(n):
+computer = [[] for _ in range(n+1)]
+visit = [False] * (n+1)
+cnt = 0
+
+for _ in range(m):
     a, b = map(int, input().split())
+    computer[a].append(b)
+    computer[b].append(a)
+
+for i in range(1, n+1):
+    computer[i].sort()
+
+print(dfs(1))

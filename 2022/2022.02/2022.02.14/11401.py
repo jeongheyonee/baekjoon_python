e1d654032fa5777249073b662deede284fadecb7
jeonghyeonee <jlh1218@naver.com>
2022-02-16 23:20:25 +0900

    2022.01.16.

--- 2022/2022.02/2022.02.14/11401.py
+++ 2022/2022.02/2022.02.14/11401.py
@@ -1,6 +1,8 @@
 # 이항계수를 어떻게 분할 정복으로 풀죠?
 # https://rhdtka21.tistory.com/123
+# https://kyun2da.github.io/2020/08/30/BinomialCoefficient/
 # 이해를 한 번 해볼게요....
+# 페르마의 소정리, 유클리드 호제법
 
 n, k = map(int, input().split())
 p = 1000000007

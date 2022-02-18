04b28711fa9fa4d64f25582a08f93067049c93ea
jeonghyeonee <jlh1218@naver.com>
2022-02-18 22:30:38 +0900

    2022.02.18. 아어렵다

--- 2022/2022.02/2022.02.14/11401.py
+++ 2022/2022.02/2022.02.14/11401.py
@@ -4,6 +4,28 @@
 # 이해를 한 번 해볼게요....
 # 페르마의 소정리, 유클리드 호제법
 
-n, k = map(int, input().split())
+N, K = map(int, input().split())
 p = 1000000007
 
+def factorial(N):
+    n = 1
+    for i in range(2, N+1):
+        n = (n*i)%p
+    return n
+
+def square(n, k):
+    if k == 0:
+        return 1
+    elif k == 1:
+        return n
+
+    tmp = square(n, k//2)
+    if k%2:
+        return tmp*tmp*n%p
+    else:
+        return tmp*tmp%p
+
+top = factorial(N)
+bot = factorial(N-K) * factorial(K) % p
+
+print(top*square(bot, p-2)%p)
